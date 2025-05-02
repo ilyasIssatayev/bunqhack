f61e1aad18847dc048727953a23f7765df08a4bc
ilyas <issatayev.ilyas@gmail.com>
2025-05-02 22:39:45 +0200

    add icons

--- vite.config.js
+++ vite.config.js
@@ -26,12 +26,12 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: 'icon-192x192.png',
+            src: 'icon-192-192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icon-512x512.png',
+            src: 'icon-512-512.png',
             sizes: '512x512',
             type: 'image/png'
           }
