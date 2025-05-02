1a7b82717aa86480adff1d8dd64fad70df1d3ff9
vovabndrnk <volodymyr@vespertool.com>
2025-05-02 22:25:04 +0200

    Add counter

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
+import { ref } from 'vue';
 
+const counter = ref(0);
+
+function clickMe() {
+  counter.value++;
+}
 </script>
 
 <template>
-  <div class="">hello</div>
+  <div>
+    <button @click="clickMe">
+      Click
+    </button>
+    {{ counter }}
+  </div>
 </template>
