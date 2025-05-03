38bef1b1afcb75a956192854d9954e9178049756
vovabndrnk <volodymyr@vespertool.com>
2025-05-03 13:36:07 +0200

    Add link

--- src/components/Notification.vue
+++ src/components/Notification.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { toast } from 'vue3-toastify';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 const poll = ref(null);
 const ID = ref(-1)
@@ -36,33 +39,26 @@ const fetchPoll = async () => {
 }
 
 const showNotification = (message) => {
-    const bgColor = '#0E2549';
-    const html = `
-    <div class="custom-toast" style="
-      background-color: ${bgColor}; 
-      color: white; 
-      padding: 12px 16px; 
-      border-radius: 8px;
-      font-size: 14px;
-    ">
-      🚀 ${message}
-    </div>
-  `;
+  const bgColor = '#0E2549';
 
-    const tot = toast.success(html, {
-        icon: false,
-        "transition": "slide",
-        position: 'top-center',
-        "theme": "dark",
-        "type": "info",
-        "closeOnClick": false,
-        "autoClose": 2000,
-        "hideProgressBar": true,
-        "dangerouslyHTMLString": true
-    });
-
-
-}
+  toast.info(`🚀 ${message}`, {
+    position: 'top-center',
+    theme: 'dark',
+    hideProgressBar: true,
+    closeOnClick: false,
+    onClick: () => {
+      router.push('/reach-the-goal');
+    },
+    dangerouslyHTMLString: true,
+    style: {
+      backgroundColor: bgColor,
+      color: 'white',
+      padding: '12px 16px',
+      borderRadius: '8px',
+      fontSize: '14px',
+    },
+  });
+};
 
 onMounted(async () => {
     fetchPoll() // call immediately on mount
